da3e5de4e824e9b49f02216c206b92ab1485efee
gaddganepola <ganeypola@gmail.com>
2025-08-12 18:14:45 +0530

    create template for user model

---
+++ src/main/java/com/ddg/SpringSecurityDemo02/model/User.java
@@ -0,0 +1,13 @@
+package com.ddg.SpringSecurityDemo02.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+}
