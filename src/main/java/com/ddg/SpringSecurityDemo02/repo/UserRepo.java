c6a187e2dd509376acbc6f4280e744e7ab53cf0a
gaddganepola <ganeypola@gmail.com>
2025-08-12 18:38:53 +0530

    add findByUsername method

--- src/main/java/com/ddg/SpringSecurityDemo02/repo/UserRepo.java
+++ src/main/java/com/ddg/SpringSecurityDemo02/repo/UserRepo.java
@@ -3,5 +3,8 @@ package com.ddg.SpringSecurityDemo02.repo;
 import com.ddg.SpringSecurityDemo02.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
