73ce48e717fed858e72f842a300a96722b3d34e8
gaddganepola <ganeypola@gmail.com>
2025-08-12 18:55:26 +0530

    complete loadUserByUsername method

--- src/main/java/com/ddg/SpringSecurityDemo02/service/UserService.java
+++ src/main/java/com/ddg/SpringSecurityDemo02/service/UserService.java
@@ -1,17 +1,35 @@
 package com.ddg.SpringSecurityDemo02.service;
 
 import com.ddg.SpringSecurityDemo02.model.User;
+import com.ddg.SpringSecurityDemo02.model.UserPrincipal;
 import com.ddg.SpringSecurityDemo02.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepo userRepo;
+
     public User addUser(User user) {
         return userRepo.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> user = userRepo.findByUsername(username);
+
+        if(user.isPresent()) {
+            return new UserPrincipal(user.get());
+        }
+
+        throw new UsernameNotFoundException("User not found");
     }
 }
