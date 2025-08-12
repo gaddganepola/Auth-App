880a12a1979c20245c2fc06846a643763269d48a
gaddganepola <ganeypola@gmail.com>
2025-08-12 18:04:19 +0530

    set up the custom login page

--- src/main/java/com/ddg/SpringSecurityDemo02/security/SecurityConfig.java
+++ src/main/java/com/ddg/SpringSecurityDemo02/security/SecurityConfig.java
@@ -1,9 +1,27 @@
 package com.ddg.SpringSecurityDemo02.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .formLogin(form -> {
+                    form.loginPage("/login");
+                })
+
+                .build();
+
+    }
+
+
+
 }
