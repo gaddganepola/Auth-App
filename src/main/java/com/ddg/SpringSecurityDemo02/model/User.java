22fd741305d33949f8cabd1617a0744f49626da9
gaddganepola <ganeypola@gmail.com>
2025-08-12 18:16:12 +0530

    add properties for user model

--- src/main/java/com/ddg/SpringSecurityDemo02/model/User.java
+++ src/main/java/com/ddg/SpringSecurityDemo02/model/User.java
@@ -1,6 +1,9 @@
 package com.ddg.SpringSecurityDemo02.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +13,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
 }
