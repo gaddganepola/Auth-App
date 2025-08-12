2451990c49f94a1e792fe5550806ee6cab4adf94
gaddganepola <ganeypola@gmail.com>
2025-08-12 18:05:16 +0530

    develop endpoint for get login page

--- src/main/java/com/ddg/SpringSecurityDemo02/controller/ContentController.java
+++ src/main/java/com/ddg/SpringSecurityDemo02/controller/ContentController.java
@@ -1,7 +1,18 @@
 package com.ddg.SpringSecurityDemo02.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContentController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+
+
 }
