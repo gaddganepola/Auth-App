4736f232fc97f25a8f7254c890a59f080fd7c178
gaddganepola <ganeypola@gmail.com>
2025-08-12 18:38:12 +0530

    disable csrf token

--- src/main/java/com/ddg/SpringSecurityDemo02/security/SecurityConfig.java
+++ src/main/java/com/ddg/SpringSecurityDemo02/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,6 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         return httpSecurity
+                .csrf(customizer -> customizer.disable())
                 .formLogin(form -> {
                     form.loginPage("/login");
                 })
