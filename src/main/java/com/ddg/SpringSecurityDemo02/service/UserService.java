59d7d8c68ad8f2cd6a049d16ab0ef75bedc46131
gaddganepola <ganeypola@gmail.com>
2025-08-12 18:39:56 +0530

    implements UserService with UserDetailsService

--- src/main/java/com/ddg/SpringSecurityDemo02/service/UserService.java
+++ src/main/java/com/ddg/SpringSecurityDemo02/service/UserService.java
@@ -3,10 +3,11 @@ package com.ddg.SpringSecurityDemo02.service;
 import com.ddg.SpringSecurityDemo02.model.User;
 import com.ddg.SpringSecurityDemo02.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepo userRepo;
