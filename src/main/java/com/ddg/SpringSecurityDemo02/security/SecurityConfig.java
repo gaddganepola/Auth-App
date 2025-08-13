5ad1885eb3a9ba13e383af464e95be0cdc7bfe26
gaddganepola <ganeypola@gmail.com>
2025-08-13 08:55:33 +0530

    give permission for user registration post request

--- src/main/java/com/ddg/SpringSecurityDemo02/security/SecurityConfig.java
+++ src/main/java/com/ddg/SpringSecurityDemo02/security/SecurityConfig.java
@@ -2,14 +2,12 @@ package com.ddg.SpringSecurityDemo02.security;
 
 import com.ddg.SpringSecurityDemo02.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -39,11 +37,12 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(customizer -> customizer.disable())
                 .formLogin(form -> {
-                    form.loginPage("/login");
+                    form.loginPage("/login").permitAll();
                 })
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/login").permitAll();
                     auth.requestMatchers("/register").permitAll();
+                    auth.requestMatchers("/user").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .build();
